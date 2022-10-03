40b3fd5234bc4a6b0d6db285fb5ac232602f4d4a
Nataliia Chapai <natalya.chapay@gmail.com>
2022-10-03 21:55:20 +0300

    edit controller

--- controllers/boards/deleteBoard.js
+++ controllers/boards/deleteBoard.js
@@ -4,11 +4,11 @@ const { Board } = require('../../models');
 
 const deleteBoard = async (req, res) => {
   const { id } = req.params;
-  const board = await Board.findOne({ id });
+  const board = await Board.findById(id);
   if (!board) {
     throw createError(400);
   }
-  await board.deleteOne({ id });
+  await board.delete({ id });
 
   res.status(200).json({
     message: 'Board deleted successfully',
