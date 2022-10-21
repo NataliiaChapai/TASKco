7b178d7d826cec48b04eba397e3a9676318136ad
Nataliia Chapai <natalya.chapay@gmail.com>
2022-10-22 00:33:21 +0300

    edit task controller

--- controllers/tasks/deleteTask.js
+++ controllers/tasks/deleteTask.js
@@ -1,14 +1,19 @@
 const { createError } = require('../../helpers');
 
-const { Task } = require('../../models');
+const { Task, Board } = require('../../models');
 
 const deleteTask = async (req, res) => {
   const { taskId } = req.params;
-  const task = await Task.findById(taskId);
+  
+  const task = await Task.findByIdAndRemove(taskId);
   if (!task) {
     throw createError(400);
   }
-  await task.delete({ taskId });
+  const { boardId } = task;
+  const board = await Board.findById(boardId);
+  const taskStatus = task.status.toLowerCase().split(' ').join('');
+  const taskCount = `taskCount.${taskStatus}`;
+  await board.updateOne({[taskCount]: board.taskCount[taskStatus] - 1});
 
   res.status(200).json({
     message: 'Task deleted successfully',
