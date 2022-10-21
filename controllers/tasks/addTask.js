4cd5d36f3ef4abea4d8d20fcc0bf10d50fda75a4
Nataliia Chapai <natalya.chapay@gmail.com>
2022-10-21 22:41:10 +0300

    edit task controller

--- controllers/tasks/addTask.js
+++ controllers/tasks/addTask.js
@@ -5,11 +5,11 @@ const addTask = async (req, res) => {
   const { boardId } = req.params;
   const taskStatus = status.toLowerCase().split(' ').join('')
   const taskCount = `taskCount.${taskStatus}`;
-  await Task.create({ name, status, boardId });
+  const task = await Task.create({ name, status, boardId });
   const board = await Board.findById(boardId);
   await board.updateOne({[taskCount]: board.taskCount[taskStatus] + 1});
   res.status(200).json({
-    message: `Task created successfully`,
+    task
   });
 };
 
